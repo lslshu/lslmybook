1f315f712372056467d05760efaba81e60949ef3
纯粹关于 <406845939@qq.com>
2019-09-27 16:42:54 +0800

    python day 1

---
+++ python学习/Day1-15/Day1/hello.py
@@ -0,0 +1,18 @@
+'''
+这是第一个python程序，因2.x版本20年停止更新
+所以学习的是python3
+根据github的python学习100天进行的学习
+
+version：0.1
+Data：2019-09-27
+
+使用Windows的小伙伴可以在命令行提示下通过下面的命令运行该程序
+python hello.py
+对于使用Linux或macOS的小伙伴可以打开终端并键入下面的命令来运行程序
+python3 hello.py
+'''
+
+print('hello world')
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
---
+++ python学习/README.md
@@ -0,0 +1,6 @@
+先根据菜鸟教程看
+https://www.runoob.com/python3/python3-tutorial
+后根据github上进行学习
+https://github.com/jackfrued/Python-100-Days
+也可以边玩游戏边学习
+https://codecombat.163.com/manage#/
