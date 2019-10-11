2239e6ce65b733756a7da1644cc42cde1cf20180
纯粹关于 <406845939@qq.com>
2019-10-11 13:48:04 +0800

    update 2019.10.11

---
+++ python学习/Day1-15/Day5/prime.py
@@ -0,0 +1,15 @@
+"""
+输出2~99之间的素数
+Date: 2019-10-11
+"""
+
+import math
+
+for num in range(2, 100):
+    is_prime = True
+    for factor in range(2, int(math.sqrt(num)) + 1):
+        if num % factor == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=' ')
---
+++ python学习/Day1-15/Day5/table.py
@@ -0,0 +1,9 @@
+"""
+输出乘法口诀表(九九表)
+Date: 2019-10-11
+"""
+
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('%d*%d=%d' % (i, j, i * j), end='\t')
+    print()
