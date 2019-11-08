c1dbf09a0d1f375fd18aea9a68c7f831e999a5e5
纯粹关于 <406845939@qq.com>
2019-11-08 17:24:19 +0800

    update 2019.11.8

--- python学习/Day1-15/Day11/csv2.py
+++ python学习/Day1-15/Day11/csv2.py
@@ -6,18 +6,37 @@ Date: 2019-11-6
 
 import csv
 
+
 class Teacher(object):
 
-	def __init__(self,name,age,title):
-		self._name = name
-		self._age = age
-		self._title = title
-		self._index = -1
+    def __init__(self, name, age, title):
+        self.__name = name
+        self.__age = age
+        self.__title = title
+        self.__index = -1
 
-	@property
-	def name(self):
-		return self._name
-	@property
-	def age(self):
-		return self._age
-	
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @property
+    def title(self):
+        return self.__title
+
+
+filename = 'teacher.csv'
+teachers = [Teacher('lsl', 38, '叫兽'), Teacher('狄仁杰', 25, '砖家')]
+
+try:
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        for teacher in teachers:
+            writer.writerow([teacher.name, teacher.age, teacher.title])
+except BaseException as e:
+    print('无法写入文件:', filename)
+else:
+    print('保存数据完成!')
