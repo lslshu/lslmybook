223299b6c97d99ca63482d682f8769a31b399fd8
纯粹关于 <406845939@qq.com>
2019-11-13 20:28:31 +0800

    update 2019.11.13

---
+++ python学习/Day1-15/Day13/asyncio2.py
@@ -0,0 +1,21 @@
+"""
+异步I/O操作 - async和await
+
+Date: 2019-11-13
+"""
+
+import asyncio
+import threading
+
+# 通过async修饰的函数不再是普通函数而是一个协程
+# 注意async和await将在Python 3.7中作为关键字出现
+async def hello():
+	print('%s: hello,world!' % threading.current_thread())
+	await asyncio.sleep(2)
+	print('%s: goodbye,world!' % threading.current_thread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(),hello()]
+#等待二个异步I/O操作执行结束
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
