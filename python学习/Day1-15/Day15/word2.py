930cad575637b3ec86a032ead957dc2b8a1adbab
纯粹关于 <406845939@qq.com>
2020-01-06 14:20:06 +0800

    update2020.1.6

---
+++ python学习/Day1-15/Day15/word2.py
@@ -0,0 +1,16 @@
+"""
+读取Word文件
+Date: 2020-1-6
+"""
+
+from docx import Document
+
+doc = Document('./res/用函数还是用复杂的表达式.docx')
+print(len(doc.paragraphs))
+print(doc.paragraphs[0].text)
+# print(doc.paragraphs[1].runs[0].text)
+
+content = []
+for para in doc.paragraphs:
+    content.append(para.text)
+print(''.join(content))
