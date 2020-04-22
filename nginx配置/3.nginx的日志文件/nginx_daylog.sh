94419ca28568b74da7539d71c5f5a92445e19f3a
纯粹关于 <406845939@qq.com>
2020-04-22 17:30:09 +0800

    update 2020.4.22

--- nginx配置/3.nginx的日志文件/nginx_daylog.sh
+++ nginx配置/3.nginx的日志文件/nginx_daylog.sh
@@ -1,16 +1,17 @@
-#/bin/bash
-
+#！bin/bash
+#切割nginx日志，需先创建backup
 #日志保存位置
-base_path='/opt/nginx/logs'
-#获取当前年信息和月信息
-log_path=$(date -d yesterday + "%Y%m")
-# 获取昨天的日信息
-day=$(date -d yesterday +"%d")
-# 按年月创建文件夹
-mkdir -p $base_path/$log_path
-# 备份昨天的日志到当月的文件夹
-mv $base_path/access.log $base_path/$log_path/access_$day.log
-# 输出备份日志文件名
-# echo $base_path/$log_path/access_$day.log
+base_path='/backup/nginx/logs'
+#备份年月日文件
+mkdir -p /backup/nginx/logs$(date +%Y)$(date +%m)$(date +%d)
+#备份日志文件到相应的
+mv /usr/local/nginx/logs/access.log /backup/nginx/logs/$(date +%Y)$(date +%m)$(date +%d)/access_$(date -d "yesterday" +"%Y%m%d").log
+mv /usr/local/nginx/logs/error.log /backup/nginx/logs/$(date +%Y)$(date +%m)$(date +%d)/error_$(date -d "yesterday" +"%Y%m%d").log
+mv /usr/local/nginx/logs/jx.access.log /backup/nginx/logs/$(date +%Y)$(date +%m)$(date +%d)/jx.access_$(date -d "yesterday" +"%Y%m%d").log
+mv /usr/local/nginx/logs/video.access.log /backup/nginx/logs/$(date +%Y)$(date +%m)$(date +%d)/video.access_$(date -d "yesterday" +"%Y%m%d").log
+mv /usr/local/nginx/logs/www.access.log /backup/nginx/logs/$(date +%Y)$(date +%m)$(date +%d)/www.access_$(date -d "yesterday" +"%Y%m%d").log
 # 通过Nginx信号量控制重读日志
-kill -USR1 `cat /opt/nginx/logs/nginx.pid`
+kill -USR1 `cat /usr/local/nginx/logs/nginx.pid`
+
+#crontab -e 配置定时任务，比如每天23:55备份
+#59  23  * * * /backup/nginx/nginx_backup.sh
