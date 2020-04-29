b7293fb23f8b7467e30e0d1fc1f8ca56a03c2654
纯粹关于 <406845939@qq.com>
2020-04-29 16:03:30 +0800

    update 2020.4.29

--- nginx配置/1.nginx的安装文件/3.nginx.sh
+++ nginx配置/1.nginx的安装文件/3.nginx.sh
@@ -1,28 +1,65 @@
-#!bin/bash
-echo "请输入你要安装的nginx版本(12,14,16,17)："
-read ver
-if [[ "$ver" -eq 12 ]]
+#！bin/bash
+#主菜单
+menu()
+{
+	clear
+	cat <<EOF
+	   #这是一个菜单
+	   1.安装nginx
+	   2.卸载nginx
+	   3.退出脚本
+EOF
+read -p "请输入数字[1-3]:" list
+case $list in
+	1)
+		nginx_install
+	;;
+	2)
+        nginx_uninstall
+    ;;
+    3)
+        echo "退出中..."
+        sleep 2
+        exit 0
+    ;;
+esac
+}
+#安装nginx
+nginx_install()
+{
+	cat <<EOF
+	###########################
+	Nginx安装版本
+	1.安装12
+	2.安装14
+	3.安装16
+	4.安装17
+EOF
+read -p "请输入数字[1-4]：" ver
+if [[ "$ver" -eq 1 ]]
  then
 	wget http://nginx.org/download/nginx-1.12.2.tar.gz
 	tar -zxvf nginx-1.12.2.tar.gz
     cd nginx-1.12.2 
-elif [[ "$ver" -eq 14 ]]
+elif [[ "$ver" -eq 2 ]]
  then
 	wget http://nginx.org/download/nginx-1.14.2.tar.gz
 	tar -zxvf nginx-1.14.2.tar.gz
     cd nginx-1.14.2 
-elif [[ "$ver" -eq 16 ]]
+elif [[ "$ver" -eq 3 ]]
  then
 	wget http://nginx.org/download/nginx-1.16.1.tar.gz
 		tar -zxvf nginx-1.16.1.tar.gz
     cd nginx-1.16.1 
-elif [[ "$ver" -eq 17 ]]
+elif [[ "$ver" -eq 4 ]]
  then
-	wget http://nginx.org/download/nginx-1.17.3.tar.gz
-	tar -zxvf nginx-1.17.3.tar.gz
-    cd nginx-1.17.3
+	wget http://nginx.org/download/nginx-1.17.10.tar.gz
+	tar -zxvf nginx-1.17.10.tar.gz
+    cd nginx-1.17.10
 else
 	echo "没有相应的版本，请重新运行脚本"
+	sleep 2
+	exit 2
 fi
 yum install gcc gcc-c++ pcre pcre-devel zlib zlib-devel openssl openssl-devel -y
 groupadd www
@@ -99,3 +136,25 @@ chkconfig --add /etc/init.d/nginx
 chmod 755 /etc/init.d/nginx
 chkconfig --add nginx
 chkconfig nginx on
+cd && rm -rf nginx*
+exit 2
+}
+
+
+
+
+#卸载nginx
+nginx_uninstall()
+{
+  ps -ef | grep nginx
+  sleep 5
+  systemctl stop nginx
+  nginxdel=$(find / -name nginx | awk '$1')
+  rm -rf $nginxdel
+}
+
+#运行函数
+while :
+do
+	menu
+done
