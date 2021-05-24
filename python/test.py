326078abb2a9d723790dbbbcf464e2a0bc2e4c9e
纯粹关于 <406845939@qq.com>
2021-05-24 16:36:24 +0800

    update 2021.5.24

--- python/test.py
+++ python/test.py
@@ -1,12 +1,29 @@
-unconfirmed_users = ['alice','brian','candace']
-confirmed_users=[]
+class Car():
+	def __init__(self,make,model,year):
+		self.make = make
+		self.model = model
+		self.year = year
+		self.odometer_reading = 0
+	def get_descriptive_name(self):
+		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+		return long_name.title()
 
-while unconfirmed_users:
-	current_user = unconfirmed_users.pop()
+	def read_odometer(self):
+		print("This car has " + str(self.odometer_reading) + " miles on it.")
 
-	print("Verifying user:" + current_user.title())
-	confirmed_users.append(current_user)
+	def update_odometer(self,mileage):
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("You can't roll back an odometer!")
 
-print("\nThe following users have been confirmed:")
-for confirmed_user in confirmed_users:
-	print(confirmed_user.title())
+	def increment_odometer(self,miles):
+		self.odometer_reading += miles
+
+class ElectricCar(Car):
+	"""docstring for ElectricCar"""
+	def __init__(self, make,model,year):
+		super().__init__(make,model,year)
+my_tesla = ElectricCar('tesla','model s', 2016)
+print(my_tesla.get_descriptive_name())
+		
